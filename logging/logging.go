ae1a9dde38b4853d6b8fe32705bc394e802b69d0
Choko <choko@curioswitch.org>
2024-05-24 16:07:20 +0900

    git add

---
+++ logging/logging.go
@@ -0,0 +1,39 @@
+package logging
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/curioswitch/go-usegcp/gcpslog"
+
+	"github.com/curioswitch/go-curiostack/config"
+)
+
+// Initialize initalizes logging for the given configuration, setting the
+// default slog handler. JSON should always be set to true in cloud
+// deployments.
+func Initialize(config config.Common) {
+	var level slog.Level
+	switch strings.ToLower(config.Logging.Level) {
+	case "debug":
+		level = slog.LevelDebug
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	case "info":
+		fallthrough
+	default:
+		level = slog.LevelInfo
+	}
+
+	var h slog.Handler
+	if config.Logging.JSON {
+		h = gcpslog.NewHandler(os.Stderr, gcpslog.Level(level))
+	} else {
+		h = slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: level})
+	}
+
+	slog.SetDefault(slog.New(h))
+}
