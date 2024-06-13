2f3687277eb93f44141aaa5da2b0ad93c00c65ae
Choko <choko@curioswitch.org>
2024-06-13 10:37:27 +0900

    Call server start in user code to allow defers to model correctly

--- server/run.go
+++ server/run.go
@@ -15,22 +15,42 @@ import (
 	"github.com/curioswitch/go-curiostack/otel"
 )
 
-// Builder allows configuring the server that will be launched by Main.
-type Builder struct {
+// Server is the configuration of the server that will be run.
+type Server struct {
 	mux *chi.Mux
+
+	conf *config.Common
+
+	startCalled bool
 }
 
 // Mux returns the [chi.Mux] that will be served and can be used to add
 // route handlers.
-func (b *Builder) Mux() *chi.Mux {
+func (b *Server) Mux() *chi.Mux {
 	return b.mux
+}
+
+// Start starts the server, listening on the configured server address for requests
+// based on its configuration. This method will block until program exit.
+func (b *Server) Start(ctx context.Context) error {
+	b.startCalled = true
+
+	srv := NewServer(b.mux, b.conf)
+	defer srv.Close()
+
+	slog.InfoContext(ctx, fmt.Sprintf("Starting server on address %v", srv.Addr))
+	if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("server: failed to start server: %w", err)
+	}
+	return nil
 }
 
 // Main is the entrypoint for starting a server using CurioStack.
 // It should be called from your main function and passed a pointer to your
 // configuration object which embeds [config.Common]. The setup callback
-// should set up handlers and such using methods on Builder to define the
-// resulting server.
+// should set up handlers and such using methods on Server to define the
+// resulting server. It must finish setup by calling Server.Start
+// to start the server.
 //
 // If you need to define default values before loading user config, they should
 // be set on the config before passing, or otherwise it is fine to just pass a
@@ -39,7 +59,7 @@ func (b *Builder) Mux() *chi.Mux {
 //
 // An exit code is returned, so the general pattern for this function will
 // be to call [os.Exit] with the result of this function.
-func Main[T config.CurioStack](conf T, confFiles fs.FS, setup func(ctx context.Context, conf T, b *Builder) error) int {
+func Main[T config.CurioStack](conf T, confFiles fs.FS, run func(ctx context.Context, conf T, b *Server) error) int {
 	ctx := context.Background()
 
 	otel.Initialize() // initialize as early as possible to instrument globals
@@ -51,20 +71,19 @@ func Main[T config.CurioStack](conf T, confFiles fs.FS, setup func(ctx context.C
 
 	logging.Initialize(&conf.GetCommon().Logging)
 
-	b := &Builder{
+	b := &Server{
 		mux: NewMux(),
+
+		conf: conf.GetCommon(),
 	}
 
-	if err := setup(ctx, conf, b); err != nil {
-		slog.Error(fmt.Sprintf("Failed to build server: %v", err))
+	if err := run(ctx, conf, b); err != nil {
+		slog.Error(fmt.Sprintf("Failed to configure server: %v", err))
 		return 1
 	}
 
-	srv := NewServer(b.mux, conf.GetCommon())
-
-	slog.InfoContext(ctx, fmt.Sprintf("Starting server on address %v", srv.Addr))
-	if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-		slog.ErrorContext(ctx, fmt.Sprintf("Failed to start server: %v", err))
+	if !b.startCalled {
+		slog.Error("Start was not called on server.Builder, it must be called in your run callback to start the server.")
 		return 1
 	}
 
